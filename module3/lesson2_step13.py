7d2a759c60016195be593f9695bdfff455810c3a
Stepan Kim <kim.stepan94@yandex.by>
2021-02-26 19:40:11 +0300

    debug script for lesson_2 step_13

--- module3/lesson2_step13.py
+++ module3/lesson2_step13.py
@@ -4,7 +4,7 @@ import unittest
 class TestAbs(unittest.TestCase):
     def test_check_registration_form1(self):
         browser = webdriver.Chrome()
-        browser.implicitly_wait(3)
+        # browser.implicitly_wait(3)
         browser.get("http://suninjuly.github.io/registration1.html")
         browser.find_element_by_css_selector("input.first[required]").send_keys("Anakin")
         browser.find_element_by_css_selector("input.second[required]").send_keys("Skywalker")
@@ -13,11 +13,11 @@ class TestAbs(unittest.TestCase):
         welcome_text_elt = browser.find_element_by_tag_name("h1")
         welcome_text = welcome_text_elt.text
         self.assertEqual(welcome_text, "Congratulations! You have successfully registered!", "Cannot complete the registration, please check all required fields")
-        browser.quit()
+        # browser.quit()
 
     def test_check_registration_form2(self):
         browser = webdriver.Chrome()
-        browser.implicitly_wait(3)
+        # browser.implicitly_wait(3)
         browser.get("http://suninjuly.github.io/registration2.html")
         browser.find_element_by_css_selector("input.first[required]").send_keys("Anakin")
         browser.find_element_by_css_selector("input.second[required]").send_keys("Skywalker")
@@ -26,7 +26,7 @@ class TestAbs(unittest.TestCase):
         welcome_text_elt = browser.find_element_by_tag_name("h1")
         welcome_text = welcome_text_elt.text
         self.assertEqual(welcome_text, "Congratulations! You have successfully registered!", "Cannot complete the registration, please check all required fields")
-        browser.quit()
+        # browser.quit()
 
 if __name__ == "__main__":
     unittest.main()
